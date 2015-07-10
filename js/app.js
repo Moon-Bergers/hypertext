c9937981f027a893704f26ffa21abbad9b281a3d
Tricia Buckenberger <tbuckenberger@gmail.com>
2015-07-10 13:55:36 -0700

    added random word selection

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 var textApp = angular.module("hypertext", []);
 
 textApp.controller("MainController", ['$scope', function($scope) {
-  $scope.wordNumber = 0;
   $scope.score = 0;
   $scope.wordArray = [
     "Cat",
@@ -10,13 +9,19 @@ textApp.controller("MainController", ['$scope', function($scope) {
     "log",
     "omgwtf"
   ];
+  $scope.random = function () {return Math.floor(Math.random() * $scope.wordArray.length);
+  };
+
+  $scope.wordNumber = $scope.random();
 
   $scope.checker = function() {
     if($scope.answer === $scope.wordArray[$scope.wordNumber]) {
       $scope.score += $scope.wordArray[$scope.wordNumber].length;
-      $scope.wordNumber++;
+      $scope.wordNumber = $scope.random();
       $scope.answer = "";
     }
   };
+
+
 
 }]);
